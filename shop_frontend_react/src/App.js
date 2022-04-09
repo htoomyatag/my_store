70a571561a31f42c8e42d4f8b6ec60e6173138f7
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2022-04-09 17:15:05 +0630

    react login work

--- shop_frontend_react/src/App.js
+++ shop_frontend_react/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Component } from 'react';
 
 import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
 
 class App extends Component {
 
@@ -29,11 +30,45 @@ class App extends Component {
     .then(user => this.setState({ user }))
   }
 
+
+  signIn = (user) => {
+    fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            user: {
+                username: user.username,
+                password: user.password
+            }
+        })
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.token){
+        localStorage.setItem('token', result.token)
+        this.setState({
+            user: result.user
+            })
+        }
+        else {
+            this.setState({
+                error: result.error
+            })
+        }
+    })
+  }
+
   render(){
     return (
           <div className="App">
-             {this.state.user.username ? <h2>Welcome {this.state.user.first_name}</h2> :
+             {this.state.user.username ? <h2>Welcome {this.state.user.first_name}</h2> :(
+             <>
+             <SignIn signIn={this.signIn} error={this.state.error} />
              <SignUp signUp={this.signUp} />
+             </>)
            }
           </div>
       );
